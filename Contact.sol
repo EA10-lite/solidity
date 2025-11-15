192458bab175b88000633c64e563628fbdfc7b51
EA10-lite <emanuelanyigor@gmail.com>
2025-11-15 08:35:10 +0100

    modifying our contact contract

--- Contact.sol
+++ Contact.sol
@@ -28,4 +28,11 @@ contract Contact {
         contacts[msg.sender][index] = contacts[msg.sender][contacts[msg.sender].length - 1];
         contacts[msg.sender].pop();
     }
+
+    function updateContact(uint index, string memory _name, string memory _phone) public {
+        require(index < contacts[msg.sender].length, "Invalid index");
+        
+        contacts[msg.sender][index].name = _name;
+        contacts[msg.sender][index].phone = _phone;
+    }
 }
